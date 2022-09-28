20bc4e46240491e54b5418b67d6db88d498a5809
huuthang2509 <danghuuthang2509@gmail.com>
2022-09-28 16:48:06 +0700

    completed without s3

--- create-stack.sh
+++ create-stack.sh
@@ -1,1 +1,7 @@
-aws cloudformation create-stack --stack-name "$1" --template-body file://"$2" --parameters file://"$3" --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --stack-name "NetworkStack" --template-body file://"network.yml" \
+--parameters file://"network_param.conf" --capabilities CAPABILITY_IAM
+
+aws cloudformation wait stack-create-complete --stack-name "NetworkStack"
+
+aws cloudformation create-stack --stack-name "ApplicationStack" --template-body file://"app_infra.yml" \
+--parameters file://"app_param.conf" --capabilities CAPABILITY_IAM
